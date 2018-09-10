504dc595efbf03a10dc608d2f879be1880ac7685
Maxwell <gengy@msoe.edu>
2018-09-10 11:47:20 -0500

    per object sss

---
+++ Assets/SeparableSubsurfaceScatter/Script/SSSObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Renderer))]
+public class SSSObject : MonoBehaviour {
+    private List<Vector4> kernel = new List<Vector4>(16);
+    public SkinData data;
+	// Use this for initialization
+	private void Awake () {
+        data.renderer = GetComponent<Renderer>();
+        SSSFunction.SetKernel(ref data, kernel);
+    }
+
+    private void OnWillRenderObject()
+    {
+        SSSFunction.UpdateSubsurface(ref SSSCamera.currentSkr, ref data, kernel);
+    }
+}
