33a2cac127dc1753ea510a983f78400f206c102c
Johannes Deml <public@deml.io>
2020-05-25 13:17:52 +0200

    Change range property
    For very large objects the range needs to be larger

--- Assets/SeparableSubsurfaceScatter/Script/SkinData.cs
+++ Assets/SeparableSubsurfaceScatter/Script/SkinData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Create SkinData", fileName = "SkinData", order = 0)]
 public class SkinData : ScriptableObject
 {
-	[Range(0, 5)]
+	[Range(0, 50)]
 	public float SubsurfaceScaler = 1f;
 	public Color SubsurfaceColor = new Color(1f, 0.446f, 0.174f, 0f);
 	public Color SubsurfaceFalloff = new Color(1f, 0.5497051f, 0.1462264f, 0f);
